d7195bffaab0947474f07ae72d33ba18c99ffa1d
Al Azad <alazad214@gmail.com>
2024-06-01 01:09:34 +0600

    splash screen added

---
+++ lib/screens/splash.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:wallpaper/screens/home_screen.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offAll(() => const HomeScreen());
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+        body: ListView(
+      children: [
+        Container(
+          height: screenSize.height / 1,
+          alignment: Alignment.center,
+          child:
+              Center(child: Image.asset("assets/images/logo.png", height: 100)),
+        )
+      ],
+    ));
+  }
+}
