4a0a19a09c0845ec8847415138cc42f4a85b229a
Al Azad <alazad214@gmail.com>
2024-06-01 11:45:36 +0600

    custom app color

---
+++ lib/utils/appcolor.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color bgcolor = Color(0xff000000);
+  static const Color sectioncolor = Color(0xff212121);
+  static const Color color_2 = Color(0xff134f5c);
+  static const Color color_4 = Color(0xff276070);
+  static const Color color_3 = Color(0xff575a65);
+  static const Color marron_ = Color(0xff4b1f1f);
+  static const Color white_1 = Color(0xffffffff);
+  static const Color white_2 = Color(0xff2C3E3A);
+  static const Color green_ = Color(0xff00360b);
+  static const Color coffie_ = Color(0xff622d2d);
+  static const Color denim_ = Color(0xff52869f);
+  static const Color denim_2 = Color(0xff3e5e70);
+  static const Color black_ = Color(0xff000000);
+  static const Color black_2 = Color(0xff656565);
+  static const Color navy_ = Color(0xff244855);
+  static const Color navy2 = Color(0xff003135);
+  static const Color yollow_ = Color(0xfffffa00);
+}
